1c411705f51eec4d06ce6b14242585d683e57114
George  Pianka <gpianka1@gmail.com>
2019-04-20 14:15:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ let passengerId = 0;
 let tripId = 0;
 
 
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+        store.drivers.push(this);
+    }
+     trips() {
+        return store.trips.filter(trip => {
+          return trip.driverId === this.id;
+            })
+     }
+    passengers(){
+      return this.trips().map(trip => {
+        return trip.passenger()
+        })
+  }
+}
