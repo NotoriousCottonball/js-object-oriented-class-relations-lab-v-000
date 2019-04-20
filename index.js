ebadfc53036d6291d25879e81f0224b843c008a3
George  Pianka <gpianka1@gmail.com>
2019-04-20 14:16:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,3 +40,23 @@ class Passenger {
     }   
 }
 
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
+    }
+    driver() {
+        return store.drivers.find(
+            function(driver) {
+                return driver.id === this.driverId;
+            }.bind(this)
+        );}
+    passenger() {
+        return store.passengers.find(
+            function(passenger) {
+                return passenger.id === this.passengerId;
+            }.bind(this)
+    );}
+}
